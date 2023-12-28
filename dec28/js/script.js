2af078726e20beae88af0e2d3d101098dc5aa642
rslivepark <rslivepark@gmail.com>
2023-12-28 23:18:35 +0900

    12/28 뉴스피드 수정

--- dec28/js/script.js
+++ dec28/js/script.js
@@ -1,20 +1,4 @@
-import '/js/jquery-3.7.1.min.js';
+import './login-join';
+import './newsfeed';
+
 console.log('script.js');
-
-
-// 메뉴 클릭 효과 (수정중..)
-const menu = document.querySelectorAll('a');
-
-function act() {
-	for (let j = 0; j < menu.length; j++) {
-		menu[j].classList.remove('active');
-	}
-	this.classList.add('active');
-}
-
-for (let i = 0; i < menu.length; i++) {
-	menu[i].addEventListener('click', act);
-}
-
-
-
